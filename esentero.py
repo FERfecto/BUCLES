cd46b304845ed2768b621e0feddb829325d99bc5
ism46414831 <ism46414831@a17.informatica.escoladeltreball.org>
2018-01-24 12:10:10 +0100

    manigga

---
+++ esentero.py
@@ -0,0 +1,17 @@
+#coding: utf-8
+
+print ("DIVISION")
+dividiendo = int(input("Escriba el dividiendo: "))
+divisor = int(input("Escriba el divisor: "))
+
+if dividiendo % divisor == 0:
+	print("es entero")
+else:
+	print("no es entero")
+ 
+
+
+
+
+
+
